faf1792f426f353cb5fd449a3aca963c8248b639
Andrew Godfroy <andrew.godfroy@killerrin.com>
2017-04-22 20:57:40 -0400

    Added BackgroundTaskHelperBase to KST

---
+++ KillerrinStudiosToolkit/BackgroundTasks/BackgroundTaskHelperBase.cs
@@ -0,0 +1,53 @@
+﻿using KillerrinStudiosToolkit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Background;
+
+namespace KillerrinStudiosToolkit.BackgroundTasks
+{
+    public abstract class BackgroundTaskHelperBase
+    {
+        public BackgroundTaskTools m_backgroundTaskTools { get; protected set; } = new BackgroundTaskTools();
+        public string TaskName { get; protected set; }
+        public string TaskEntryPoint { get; protected set; }
+
+
+        public BackgroundTaskHelperBase(string taskName, string taskEntryPoint) {
+            TaskName = taskName;
+            TaskEntryPoint = taskEntryPoint;
+        }
+
+        public async Task<BackgroundTaskRegistration> RegisterBackgroundTask()
+        {
+            // Allow the deriving class to determine whether or not to cancel the registration
+            if (CancelTaskRegistration()) return null;
+
+            // Check if the Task already exists and unregister it if it does
+            UnregisterTask();
+
+            // Ensure the app has permission to run the task
+            var access = await m_backgroundTaskTools.RequestAccess();
+            if (!access) return null;
+
+            // Build the Task...
+            var builder = BuildTask();
+
+            // ...Then Register It
+            BackgroundTaskRegistration task = builder.Register();
+            return task;
+        }
+
+        protected abstract BackgroundTaskBuilder BuildTask();
+        protected abstract bool CancelTaskRegistration();
+
+        public void UnregisterTask()
+        {
+            var existingTask = m_backgroundTaskTools.GetRegisteredTask(TaskName);
+            if (existingTask != null)
+                existingTask.Unregister(false);
+        }
+    }
+}
---
+++ KillerrinStudiosToolkit/BackgroundTasks/BackgroundTaskTools.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Background;
+
+namespace KillerrinStudiosToolkit.BackgroundTasks
+{
+    public class BackgroundTaskTools
+    {
+        public bool IsTaskRegistered(string taskName)
+        {
+            return GetRegisteredTask(taskName) != null;
+        }
+        public IBackgroundTaskRegistration GetRegisteredTask(string taskName)
+        {
+            foreach (var task in BackgroundTaskRegistration.AllTasks)
+            {
+                if (task.Value.Name == taskName)
+                {
+                    return task.Value;
+                }
+            }
+
+            return null;
+        }
+
+        #region GetAccessStatus
+        public BackgroundAccessStatus GetAccessStatus() => BackgroundExecutionManager.GetAccessStatus();
+        public BackgroundAccessStatus GetAccessStatus(string applicationID) => BackgroundExecutionManager.GetAccessStatus(applicationID); 
+        #endregion
+
+        #region RemoveAccess
+        public void RemoveAccess() => BackgroundExecutionManager.RemoveAccess();
+        public void RemoveAccess(string applicationID) => BackgroundExecutionManager.RemoveAccess(applicationID);
+        #endregion
+
+        #region IsAccessAllowed
+        public bool IsAccessAllowed()
+        {
+            var backgroundTaskStatus = BackgroundExecutionManager.GetAccessStatus();
+            return IsAccessAllowed(backgroundTaskStatus);
+        }
+        public bool IsAccessAllowed(string applicationID)
+        {
+            var backgroundTaskStatus = BackgroundExecutionManager.GetAccessStatus(applicationID);
+            return IsAccessAllowed(backgroundTaskStatus);
+        }
+        public bool IsAccessAllowed(BackgroundAccessStatus accessStatus)
+        {
+            if (accessStatus == BackgroundAccessStatus.Denied ||
+                accessStatus == BackgroundAccessStatus.DeniedBySystemPolicy ||
+                accessStatus == BackgroundAccessStatus.DeniedByUser ||
+                accessStatus == BackgroundAccessStatus.Unspecified)
+                return false;
+            return true;
+        } 
+        #endregion
+
+        #region Request Access
+        public async Task<bool> RequestAccess()
+        {
+            if (IsAccessAllowed())
+                return true;
+
+            var result = await BackgroundExecutionManager.RequestAccessAsync();
+            return IsAccessAllowed();
+        }
+        public async Task<bool> RequestAccess(string applicationID)
+        {
+            if (IsAccessAllowed())
+                return true;
+
+            var result = await BackgroundExecutionManager.RequestAccessAsync(applicationID);
+            return IsAccessAllowed();
+        } 
+        #endregion
+
+    }
+}
